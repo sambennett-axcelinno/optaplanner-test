91d5ca07bca3fe79e6a9e381df2040234be7669a
Sam <sam.bennett@axcelinno.io>
2021-03-09 09:57:57 -0600

    testing another method

--- src/main/java/com/sam/constraintProvider/LectureConstraintProvider.java
+++ src/main/java/com/sam/constraintProvider/LectureConstraintProvider.java
@@ -13,9 +13,12 @@ public class LectureConstraintProvider implements ConstraintProvider {
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
         return new Constraint[] {
                 roomConflict(constraintFactory),
-                teacherConflict(constraintFactory),
+                //teacherConflict(constraintFactory),
+                periodConflict(constraintFactory),
                 teacherConflictBennett(constraintFactory),
-                teacherConflictHulk(constraintFactory)
+                teacherConflictHulk(constraintFactory),
+                teacherPrefersSameRoom(constraintFactory),
+                preferBackToBack(constraintFactory)
         };
     }
 
@@ -29,6 +32,12 @@ public class LectureConstraintProvider implements ConstraintProvider {
         return constraintFactory.fromUniquePair(Lecture.class,
                 Joiners.equal(Lecture::getRoomNumber), Joiners.equal(Lecture::getTeacher))
                 .penalize("Teacher Conflict", HardSoftScore.ONE_HARD);
+    }
+
+    Constraint periodConflict(ConstraintFactory constraintFactory) {
+        return constraintFactory.fromUniquePair(Lecture.class,
+                Joiners.equal(Lecture::getTeacher), Joiners.equal(Lecture::getPeriod))
+                .penalize("Period Conflict", HardSoftScore.ONE_HARD);
     }
 
     Constraint teacherConflictBennett(ConstraintFactory constraintFactory) {
@@ -45,5 +54,19 @@ public class LectureConstraintProvider implements ConstraintProvider {
                         Joiners.equal(Lecture::getTeacher))
                 .filter(((lecture, lecture2) -> {return (lecture.getTeacher().getName().equals("Hulk") && lecture.getPeriod().equals(1));}))
                 .penalize("Does not want period 1", HardSoftScore.ONE_SOFT);
+    }
+
+    Constraint teacherPrefersSameRoom(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(Lecture.class)
+                .join(Lecture.class, Joiners.equal(Lecture::getTeacher))
+                .filter((lecture, lecture2) -> {return lecture.getTeacher().equals(lecture2.getTeacher()) && !lecture.getRoomNumber().equals(lecture2.getRoomNumber());})
+                .penalize("Prefer same room", HardSoftScore.ONE_SOFT);
+    }
+
+    Constraint preferBackToBack(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(Lecture.class)
+                .join(Lecture.class, Joiners.equal(Lecture::getTeacher), Joiners.equal(Lecture::getPeriod))
+                .filter((lecture, lecture2) -> {return lecture.getTeacher().equals(lecture2.getTeacher()) && (Math.abs(lecture.getPeriod() - lecture2.getPeriod()) != 1);})
+                .penalize("Prefer back to back", HardSoftScore.ONE_SOFT);
     }
 }
